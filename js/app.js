58934f5ed6f30ff629d26d86e3b3122349f2c42e
unknown <johnvictordml@gmail.com>
2024-04-18 00:20:25 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navList = document.querySelector('.nav-list');
+  
+    menuBtn.addEventListener('click', function () {
+      navList.classList.toggle('show');
+    });
+  });
+  
+  

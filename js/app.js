965a257173868bdad9e77465c6d70733d28a132c
unknown <johnvictordml@gmail.com>
2024-04-18 11:22:15 +0100

    done with some pages

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.querySelector('.menu-btn');
-    const navList = document.querySelector('.nav-list');
-  
-    menuBtn.addEventListener('click', function () {
-      navList.classList.toggle('show');
-    });
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.querySelector('.nav-list').classList.toggle('active');
+});
+
+
+// Gallery Modal
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalImg");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+var images = document.querySelectorAll(".gallery-image-cont img");
+
+images.forEach(function(img, index) {
+  img.addEventListener("click", function() {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
   });
-  
-  
+});
+
+
+// // to close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// };

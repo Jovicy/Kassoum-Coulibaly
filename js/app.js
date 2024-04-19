30781a99aeedaa0541790fcca9053dc8605c4096
unknown <johnvictordml@gmail.com>
2024-04-20 00:21:19 +0100

    making it a usual one

--- js/app.js
+++ js/app.js
@@ -22,4 +22,18 @@ images.forEach(function(img, index) {
 // to close the modal
 span.onclick = function() {
   modal.style.display = "none";
-};
+};7
+
+// JavaScript for Submenu
+const submenuItems = document.querySelectorAll('.has-submenu');
+
+submenuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('open');
+    });
+});
+
+// google translate code
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: "en,fr,ar,es", layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
